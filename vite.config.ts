5033c8a7929059e6e108bf2f4110b85ea2c46bcb
Zerone <zhengpeixin@bingosoft.net>
2024-07-10 10:28:28 +0800

    build: update or delete external dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer()],
-    },
-  },
   build: {
     copyPublicDir: false,
     rollupOptions: {
